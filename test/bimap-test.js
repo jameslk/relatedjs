30a27a6f63de25db52dc3c75299296b20f4941f9
James Koshigoe <james@jameskoshigoe.com>
2015-09-12 22:58:19 -0600

    Added tested merging capabilities to Bimap, for use with Graph merging

--- test/bimap-test.js
+++ test/bimap-test.js
@@ -11,148 +11,270 @@ describe('Bimap', () => {
     });
 
     it('can set a key to a value', () => {
-        bimap.set('a', 'b');
+        bimap.set('apple', 'triangle');
 
-        expect(keyValues('a')).to.eql(['b']);
-        expect(valueKeys('b')).to.eql(['a']);
+        expect(keyValues('apple')).to.eql(['triangle']);
+        expect(valueKeys('triangle')).to.eql(['apple']);
     });
 
     it('can replace a key and value', () => {
-        bimap.set('a', 'b');
-        bimap.set('a', 'c');
+        bimap.set('apple', 'triangle');
+        bimap.set('apple', 'circle');
 
-        expect(keyValues('a')).to.eql(['c']);
-        expect(valueKeys('c')).to.eql(['a']);
-        expect(valueKeys('b')).to.be.undefined;
+        expect(keyValues('apple')).to.eql(['circle']);
+        expect(valueKeys('circle')).to.eql(['apple']);
+        expect(valueKeys('triangle')).to.be.undefined;
 
-        bimap.set('a2', 'c');
+        bimap.set('orange', 'circle');
 
-        expect(valueKeys('c')).to.eql(['a2']);
-        expect(keyValues('a')).to.be.undefined;
+        expect(valueKeys('circle')).to.eql(['orange']);
+        expect(keyValues('apple')).to.be.undefined;
     });
 
     it('can append a key and value', () => {
-        bimap.append('a1', 'b');
-        bimap.append('a1', 'c');
-        bimap.append('a2', 'c');
-        bimap.append('a2', 'd');
-        bimap.append('a3', 'd');
+        bimap.append('lemon', 'triangle');
+        bimap.append('lemon', 'circle');
+        bimap.append('orange', 'circle');
+        bimap.append('orange', 'heart');
+        bimap.append('melon', 'heart');
 
-        expect(keyValues('a1')).to.eql(['b', 'c']);
-        expect(keyValues('a2')).to.eql(['c', 'd']);
-        expect(keyValues('a3')).to.eql(['d']);
+        expect(keyValues('lemon')).to.eql(['triangle', 'circle']);
+        expect(keyValues('orange')).to.eql(['circle', 'heart']);
+        expect(keyValues('melon')).to.eql(['heart']);
 
-        expect(valueKeys('b')).to.eql(['a1']);
-        expect(valueKeys('c')).to.eql(['a1', 'a2']);
-        expect(valueKeys('d')).to.eql(['a2', 'a3']);
+        expect(valueKeys('triangle')).to.eql(['lemon']);
+        expect(valueKeys('circle')).to.eql(['lemon', 'orange']);
+        expect(valueKeys('heart')).to.eql(['orange', 'melon']);
     });
 
     it('can append a value to a key', () => {
-        bimap.set('a', 'b');
-        bimap.appendValueToKey('a', 'c');
-        bimap.appendValueToKey('a', 'd');
+        bimap.set('apple', 'triangle');
+        bimap.appendValueToKey('apple', 'circle');
+        bimap.appendValueToKey('apple', 'heart');
 
-        expect(keyValues('a')).to.eql(['b', 'c', 'd']);
-        expect(valueKeys('b')).to.eql(['a']);
-        expect(valueKeys('c')).to.eql(['a']);
-        expect(valueKeys('d')).to.eql(['a']);
+        expect(keyValues('apple')).to.eql(['triangle', 'circle', 'heart']);
+        expect(valueKeys('triangle')).to.eql(['apple']);
+        expect(valueKeys('circle')).to.eql(['apple']);
+        expect(valueKeys('heart')).to.eql(['apple']);
     });
 
     it('can append a key to a value', () => {
-        bimap.set('a', 'b');
-        bimap.appendKeyToValue('b', 'c');
-        bimap.appendKeyToValue('b', 'd');
+        bimap.set('apple', 'triangle');
+        bimap.appendKeyToValue('triangle', 'circle');
+        bimap.appendKeyToValue('triangle', 'heart');
 
-        expect(keyValues('a')).to.eql(['b']);
-        expect(valueKeys('b')).to.eql(['a', 'c', 'd']);
-        expect(keyValues('c')).to.eql(['b']);
-        expect(keyValues('d')).to.eql(['b']);
+        expect(keyValues('apple')).to.eql(['triangle']);
+        expect(valueKeys('triangle')).to.eql(['apple', 'circle', 'heart']);
+        expect(keyValues('circle')).to.eql(['triangle']);
+        expect(keyValues('heart')).to.eql(['triangle']);
     });
 
     it('can get values for a key', () => {
-        bimap.append('a', 'b');
-        bimap.append('a', 'c');
-        bimap.append('a', 'd');
+        bimap.append('apple', 'triangle');
+        bimap.append('apple', 'circle');
+        bimap.append('apple', 'heart');
 
-        expect(keyValues('a')).to.eql(['b', 'c', 'd']);
+        expect(keyValues('apple')).to.eql(['triangle', 'circle', 'heart']);
     });
 
     it('can get keys for a value', () => {
-        bimap.append('a', 'b');
-        bimap.append('c', 'b');
-        bimap.append('d', 'b');
+        bimap.append('apple', 'triangle');
+        bimap.append('circle', 'triangle');
+        bimap.append('heart', 'triangle');
 
-        expect(valueKeys('b')).to.eql(['a', 'c', 'd']);
+        expect(valueKeys('triangle')).to.eql(['apple', 'circle', 'heart']);
     });
 
     it('can remove a key to a value', () => {
-        bimap.append('a', 'b');
-        bimap.append('a', 'c');
-        bimap.append('a2', 'c');
-        bimap.append('a2', 'd');
-        bimap.append('a3', 'd');
+        bimap.append('apple', 'triangle');
+        bimap.append('apple', 'circle');
+        bimap.append('orange', 'circle');
+        bimap.append('orange', 'heart');
+        bimap.append('melon', 'heart');
 
-        bimap.remove('a', 'c');
-        bimap.remove('a3', 'd');
+        bimap.remove('apple', 'circle');
+        bimap.remove('melon', 'heart');
 
-        expect(keyValues('a')).to.eql(['b']);
-        expect(keyValues('a2')).to.eql(['c', 'd']);
-        expect(keyValues('a3')).to.be.undefined;
+        expect(keyValues('apple')).to.eql(['triangle']);
+        expect(keyValues('orange')).to.eql(['circle', 'heart']);
+        expect(keyValues('melon')).to.be.undefined;
 
-        expect(valueKeys('b')).to.eql(['a']);
-        expect(valueKeys('c')).to.eql(['a2']);
-        expect(valueKeys('d')).to.eql(['a2']);
+        expect(valueKeys('triangle')).to.eql(['apple']);
+        expect(valueKeys('circle')).to.eql(['orange']);
+        expect(valueKeys('heart')).to.eql(['orange']);
     });
 
     it('can remove a key', () => {
-        bimap.append('a', 'b');
-        bimap.append('a2', 'b');
-        bimap.append('a', 'c');
-        bimap.append('a2', 'c');
+        bimap.append('apple', 'triangle');
+        bimap.append('orange', 'triangle');
+        bimap.append('apple', 'circle');
+        bimap.append('orange', 'circle');
 
-        bimap.removeKey('a');
+        bimap.removeKey('apple');
 
-        expect(keyValues('a')).to.be.undefined;
-        expect(keyValues('a2')).to.eql(['b', 'c']);
+        expect(keyValues('apple')).to.be.undefined;
+        expect(keyValues('orange')).to.eql(['triangle', 'circle']);
 
-        expect(valueKeys('b')).to.eql(['a2']);
-        expect(valueKeys('c')).to.eql(['a2']);
+        expect(valueKeys('triangle')).to.eql(['orange']);
+        expect(valueKeys('circle')).to.eql(['orange']);
     });
 
     it('can remove a value', () => {
-        bimap.append('a', 'b');
-        bimap.append('a2', 'b');
-        bimap.append('a', 'c');
-        bimap.append('a2', 'c');
+        bimap.append('apple', 'triangle');
+        bimap.append('orange', 'triangle');
+        bimap.append('apple', 'circle');
+        bimap.append('orange', 'circle');
 
-        bimap.removeValue('c');
+        bimap.removeValue('circle');
 
-        expect(keyValues('a')).to.eql(['b']);
-        expect(keyValues('a2')).to.eql(['b']);
+        expect(keyValues('apple')).to.eql(['triangle']);
+        expect(keyValues('orange')).to.eql(['triangle']);
 
-        expect(valueKeys('b')).to.eql(['a', 'a2']);
-        expect(valueKeys('c')).to.be.undefined;
+        expect(valueKeys('triangle')).to.eql(['apple', 'orange']);
+        expect(valueKeys('circle')).to.be.undefined;
     });
 
     it('can convert to a literal object', () => {
-        bimap.append('a', 'b');
-        bimap.append('a2', 'b');
-        bimap.append('a', 'c');
-        bimap.append('a2', 'c');
+        bimap.append('apple', 'triangle');
+        bimap.append('orange', 'triangle');
+        bimap.append('apple', 'circle');
+        bimap.append('orange', 'circle');
 
         expect(bimap.toLiteral()).to.eql({
-            keys: {'a': ['b', 'c'], 'a2': ['b', 'c']},
-            values: {'b': ['a', 'a2'], 'c': ['a', 'a2']}
+            keys: {'apple': ['triangle', 'circle'], 'orange': ['triangle', 'circle']},
+            values: {'triangle': ['apple', 'orange'], 'circle': ['apple', 'orange']}
         });
     });
 
-    function keyValues(key) {
-        const keyValues = bimap.getKeyValues(key);
+    describe('with two Bimaps', () => {
+        let bimap1, bimap2;
+
+        beforeEach(() => {
+            bimap1 = new Bimap();
+            bimap2 = new Bimap();
+
+            // foo grouping
+            bimap1.set('foo_apple', 'foo_triangle');
+
+            // ->
+
+            bimap2.set('foo_apple', 'foo_circle');
+            bimap2.set('foo_orange', 'foo_heart');
+
+            // bar grouping
+            bimap1.set('bar_apple', 'bar_triangle');
+            bimap1.set('bar_orange', 'bar_circle');
+            bimap1.set('bar_lemon', 'bar_diamond');
+
+            // ->
+
+            bimap2.set('bar_apple', 'bar_circle');
+            bimap2.append('bar_apple', 'bar_heart');
+            bimap2.set('bar_melon', 'bar_diamond');
+        });
+
+        it('creates a new Bimap of merged keys and values', () => {
+            let {keys, values} = Bimap.merge(bimap1, bimap2).toLiteral();
+
+            expectLiteralsToMatch(keys, {
+                foo_apple: ['foo_triangle', 'foo_circle'],
+                foo_orange: ['foo_heart'],
+
+                bar_apple: ['bar_triangle', 'bar_circle', 'bar_heart'],
+                bar_orange: ['bar_circle'],
+                bar_lemon: ['bar_diamond'],
+                bar_melon: ['bar_diamond']
+            });
+
+            expectLiteralsToMatch(values, keysToValues(keys));
+        });
+
+        it('creates a new Bimap of the former keys and values replaced with the latter', () => {
+            let {keys, values} = Bimap.replace(bimap1, bimap2).toLiteral();
+
+            expect(keys).to.eql({
+                foo_apple: ['foo_circle'],
+                foo_orange: ['foo_heart'],
+
+                bar_apple: ['bar_circle', 'bar_heart'],
+                bar_melon: ['bar_diamond']
+            });
+
+            expectLiteralsToMatch(keysToValues(keys), values);
+        });
+
+        it('creates a new Bimap of merged keys and replaced values', () => {
+            let {keys, values} = Bimap.mergeKeysReplaceValues(bimap1, bimap2).toLiteral();
+
+            expectLiteralsToMatch(keys, {
+                foo_apple: ['foo_triangle', 'foo_circle'],
+                foo_orange: ['foo_heart'],
+
+                bar_apple: ['bar_triangle', 'bar_circle', 'bar_heart'],
+                bar_melon: ['bar_diamond']
+            });
+
+            expectLiteralsToMatch(values, keysToValues(keys));
+        });
+
+        it('creates a new Bimap of replaced keys and merged values', () => {
+            let {keys, values} = Bimap.replaceKeysMergeValues(bimap1, bimap2).toLiteral();
+
+            expect(keys).to.eql({
+                foo_apple: ['foo_circle'],
+                foo_orange: ['foo_heart'],
+
+                bar_apple: ['bar_circle', 'bar_heart'],
+                bar_orange: ['bar_circle'],
+                bar_lemon: ['bar_diamond'],
+                bar_melon: ['bar_diamond']
+            });
+
+            expectLiteralsToMatch(keysToValues(keys), values);
+        });
+    });
+
+    function keyValuesFor(forBimap, key) {
+        const keyValues = forBimap.getKeyValues(key);
         return keyValues ? Array.from(keyValues) : keyValues;
     }
 
-    function valueKeys(value) {
-        const valueKeys = bimap.getValueKeys(value);
+    function valueKeysFor(forBimap, value) {
+        const valueKeys = forBimap.getValueKeys(value);
         return valueKeys ? Array.from(valueKeys) : valueKeys;
+    }
+
+    function keyValues(key) {
+        return keyValuesFor(bimap, key);
+    }
+
+    function valueKeys(value) {
+        return valueKeysFor(bimap, value);
+    }
+
+    function keysToValues(keysObject) {
+        let valuesObject = {};
+
+        Object.keys(keysObject).forEach(key => {
+            const values = keysObject[key];
+
+            values.forEach(value => {
+                if (valuesObject.hasOwnProperty(value)) {
+                    valuesObject[value].push(key);
+                } else {
+                    valuesObject[value] = [key];
+                }
+            });
+        });
+
+        return valuesObject;
+    }
+
+    function expectLiteralsToMatch(literal1, literal2) {
+        expect(literal1).to.have.all.keys(literal2);
+
+        Object.keys(literal1).forEach(key => {
+            expect(literal1[key]).to.have.members(literal2[key]);
+        });
     }
 });
